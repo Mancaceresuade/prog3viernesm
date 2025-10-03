ee4b502a82a9021c8462f64eebfd503b512bdb99
Mancaceresuade <mancaceres@uade.edu.ar>
2025-10-03 11:26:07 -0300

    tp ejemplo, conexion neo4j

---
+++ clase9/demo/demo/src/main/java/com/ejercicio/demo/repo/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.ejercicio.demo.repo;
+
+import com.ejercicio.demo.model.MovieEntity;
+import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
+import reactor.core.publisher.Mono;
+
+public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity,
+        String> {
+    Mono<MovieEntity> findOneByTitle(String title);
+}
